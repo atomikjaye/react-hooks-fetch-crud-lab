ea34eae67da8c99b36dc86f92ccae2b190be948d
Jocelyn Naarden <tzionone@gmail.com>
2022-06-03 19:51:03 -0400

    Get Request Completed

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem"
 
 function QuestionList() {
+  const [questionsArr, setQuestionsArr] = useState([])
+
+  // Fetch using useEFFECT because user isn't starting the request
+  useEffect(() => {
+    fetch(`http://localhost:4000/questions`)
+      .then(r => r.json())
+      .then(questions => setQuestionsArr(questions))
+  }, [])
+
+  const displayQuestions = questionsArr.map((question) => {
+    return (
+      <QuestionItem
+        key={question.id}
+        question={question}
+      />
+    )
+  })
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{displayQuestions}</ul>
     </section>
   );
 }
